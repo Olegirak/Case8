276b42c8f25f6147cb46b73e2c4fea6500fc4628
Oleg Iraklionov <irakliov@mail.ru>
2022-12-20 18:12:48 +0300

    Case 8 is ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу , на вход принимающую число Н, а на выходе все четные
+int N = int.Parse(Console.ReadLine());
+
+for (int i=1;i<N+1;i++)
+    {
+        if( i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        
+    }
